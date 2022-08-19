c83d253be43953e462e06466ab70c6e00573c20e
Yuan-Dell <yuanhao524@gmail.com>
2022-08-19 22:11:04 +0800

    Fix base64 image prefix

--- src/features/GenerateStructuralReport/GenerateStructuralReportHelper.tsx
+++ src/features/GenerateStructuralReport/GenerateStructuralReportHelper.tsx
@@ -8,24 +8,24 @@ export const generateReport = async (structuralData: Array<StructuralData>, call
     reader.onload = async () => {
         let template = reader.result;
         structuralData = structuralData.map(d => {
+            d.Image64 = d.Image64.includes('data:image/png;base64,') ? d.Image64 : `data:image/png;base64,${d.Image64}`;
             const img = new Image();
-            console.log(d.Image64);
             img.src = d.Image64;
             document.body.appendChild(img);
             let ratio = img.width / img.height || 4 / 3;
             d['Image64Ratio'] = ratio;
             document.body.removeChild(img);
-            console.log(ratio);
             return d;
         });
 
         console.log('Start generating report');
+        console.log(structuralData);
         try {
             downloadBlob(
                 'report.docx',
                 await dataToReport(structuralData, template),
                 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-                callback(),
+                callback,
             );
         } catch (err) {
             console.log(err);
